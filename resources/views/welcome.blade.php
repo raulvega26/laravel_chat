4d5ecb63103ab6bd77fa003635145b40fc626305
raulvega26 <raul.vega86@gmail.com>
2020-02-11 12:28:19 +0100

    created basic form and css file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,64 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>init session</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> -->
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" />
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -79,20 +28,23 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="content">
+                <div class="prueba">
+                    <form>
+                    	<table>
+							<span class="form-group">
+								<tr><td><label for="exampleInputEmail1">Nombre de usuario</label></td>
+								<td><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Escribe tu nombre usuario"></td></tr>
+								
+							</span>
+							<span class="form-group">
+								<tr><td><label for="exampleInputPassword1">Foto</label></td>
+								<td><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Inserta una foto"></tr>
+							<tr><td></td><td><button type="submit" class="btn btn-primary">Iniciar sesión</button></td></tr>
+						</span>
+						</table>
+					</form>
                 </div>
             </div>
         </div>
